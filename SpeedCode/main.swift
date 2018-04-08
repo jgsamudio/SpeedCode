ac8b54b5f33a4bcfdc5c95d81da887f02356c3f0
jgsamudio <jonathan2457@gmail.com>
2018-04-08 17:37:02 -0400

    Added project.

---
+++ SpeedCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SpeedCode
+//
+//  Created by Jonathan Samudio on 4/8/18.
+//  Copyright © 2018 JustBinary. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
